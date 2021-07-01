6d58ee3fb35d681b45a66c5138c80b5d8307148b
Falcom <benduplay@gmail.com>
2021-07-01 13:26:13 +0200

    index redesign

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -21,14 +21,14 @@ function tableauProducts(productsList) {
     const mainProduct = document.getElementById("main")
     productsList.forEach(productList => {
         const divProduct = document.createElement("div")
-        divProduct.classList.add("col-12", "col-md-6", "col-lg-4", "mb-3")
+        divProduct.classList.add("col-12", "col-md-6", "col-lg-4", "mb-5")
         divProduct.innerHTML = `            
-          <div class="card mb-4 mb-lg-0 border-primary shadow">
-              <img src="${productList.imageUrl}" alt="${productList.name}" class="card-img-top">
+          <div class="card mb-4 mb-lg-0 rounded shadow bg_Orinoco">
+              <img src="${productList.imageUrl}" alt="${productList.name}" class="card-img-top pt-3 px-3">
               <div class="card-body">
-              <h3 class="card-title h5">${productList.name}</h3>
+              <h2 class="card-title h5">${productList.name}</h2>
               <p class="card-text">Prix : ${productList.price / 100}.00 €</p>
-              <a href="product.html?id=${productList._id}" class="btn btn-primary btn-block stretched-link">Adopter ${productList.name} ?</a>
+              <a href="product.html?id=${productList._id}" class="btn btn-Orinoco btn-block stretched-link">Adopter ${productList.name} ?</a>
               </div>
           </div>`
         mainProduct.appendChild(divProduct)
